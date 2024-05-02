06434945187dac5b9cb5af129ab3c01bfb7692ae
Estiven <147244932+camachoo18@users.noreply.github.com>
2024-05-02 15:08:39 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float speedPlayer = 10f;
-    [SerializeField] float friction = 5f;
     Rigidbody rb;
     float maxVelocity = 10f;
     Vector3 rotate = new Vector3(0, 0.1f, 0);
+
+    [Header("Friction y break")]
+    [SerializeField] float breakPower;
+    [SerializeField] float friction;
+
+    [Header("Particulas")]
     [SerializeField] ParticleSystem particulasAC;
-    [SerializeField] ParticleSystem left;
-    [SerializeField] ParticleSystem right;
+    [SerializeField] ParticleSystem particulasleft;
+    [SerializeField] ParticleSystem particulasRight;
 
     void Start()
     {
         particulasAC.Stop();
+        particulasleft.Stop();
+        particulasRight.Stop();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -23,58 +30,90 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 movement = Vector3.zero;
 
+       
         if (Input.GetKey(KeyCode.W))
         {
-            movement.z -= 1;
-            //movement += transform.forward;
-            
+            movement.z += 1;
+            print("a");
         }
 
         if (Input.GetKeyDown(KeyCode.W))
             particulasAC.Play();
 
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            // movement.x -= 1;
+//break and backward
 
+        if (Input.GetKeyDown(KeyCode.D))
+            particulasleft.Play();
+
+        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            particulasleft.Stop();
+            particulasRight.Stop();
         }
+
+        if (Input.GetKeyDown(KeyCode.A))
+            particulasRight.Play();
+
+
+
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    if(movement != Vector3.zero)
+        //    {
+        //        rb.velocity = new Vector3(
+        //                rb.velocity.x / ( 1 + frenado * Time.deltaTime),
+        //                rb.velocity.y,
+        //                (rb.velocity.z / (1 + frenado * Time.deltaTime)));
+        //        print("s");
+        //    }
+
+        //    else
+        //    {
+        //        rb.AddForce(-transform.forward * -speedPlayer * Time.deltaTime);
+        //    }
+        //    print(movement);
+        //}
 
         if (Input.GetKey(KeyCode.D))
         {
             rb.angularVelocity += rotate;
         }
-        if (Input.GetKeyDown(KeyCode.D))
-            right.Play();
-        
-        
-        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
-        {
-            left.Stop();
-            right.Stop();
-        }
-
 
         if (Input.GetKey(KeyCode.A))
         {
             rb.angularVelocity -= rotate;
 
         }
-        if (Input.GetKeyDown(KeyCode.A))
-            left.Play();
 
 
         if (movement != Vector3.zero)
+        {
             rb.velocity += transform.forward * speedPlayer * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = new Vector3(
+                    rb.velocity.x / (1 + breakPower * Time.deltaTime),
+                    rb.velocity.y,
+                    (rb.velocity.z / (1 + breakPower * Time.deltaTime)));
+                print("Estoy Frenando");
+            }
+        }
 
         else
         {
-            //rb.velocity /= rb.velocity.z + friccion * Time.deltaTime;
-            rb.velocity = new Vector3(rb.velocity.x / (1 + friction * Time.deltaTime),
+            rb.velocity = new Vector3(
+                rb.velocity.x / (1 + friction * Time.deltaTime),
                 rb.velocity.y,
                 (rb.velocity.z / (1 + friction * Time.deltaTime)));
-           //print(friccion);
+            print(friction);
             particulasAC.Stop();
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity -= transform.forward * speedPlayer * Time.deltaTime;
+                print("patras");
+            }
         }
 
 
