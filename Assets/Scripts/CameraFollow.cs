1c92dcaee849a1da4e7fe8d081be94fb7cbde76e
Estiven <147244932+camachoo18@users.noreply.github.com>
2024-05-08 20:25:39 +0200

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,32 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] Transform PlayerTr;
-    [SerializeField] float followPlayerSmooth;
-    [SerializeField] float lookAheadDistance;
-    [SerializeField] float lookAheadSmooth;
-    [SerializeField] float rotationSmooth;
-    [SerializeField] float minDistanceToPlayer;
-    [SerializeField] float upDistance;
-    [SerializeField] Vector3 offset = new Vector3(10f, 10f, 10f);
-    [SerializeField] Vector3 currenteVelocity;
+    [SerializeField] Transform target;
+
+    [SerializeField] float followSmooth = .3f;
+    [SerializeField] float lookAheadDistance = 2;
+    [SerializeField] float lookAheadSmooth = 1;
+    [SerializeField] float rotationSmooth = 1;
+    [SerializeField] float minDistanceToTarget = 5;
+
+    Vector3 positionVelocity = Vector3.zero;
+    Vector3 lookAhead;
+    bool playerIsDead = false;
+    Vector3 lookAheadVelocity = Vector3.zero;
+    Vector3 directionToTarget;
+
+
     void Start()
     {
+        transform.position = target.position;
 
+      
     }
 
-    void Update()
+
+     void Update()
     {
-        transform.position = Vector3.SmoothDamp(
-            transform.localPosition,
-            PlayerTr.position + PlayerTr.forward * offset.z + PlayerTr.up * upDistance + PlayerTr.right * lookAheadDistance,
-            ref currenteVelocity,
-            followPlayerSmooth
-        );
-        transform.LookAt(PlayerTr.position);
-        //transform.rotation = Quaternion.AngleAxis(Input.GetAxis("Horizontal") * rotationSmooth, Vector3.up) * offset;
+        if (!playerIsDead)
+        {
+            transform.position = Vector3.SmoothDamp(
+                transform.position,
+                target.position - target.forward * minDistanceToTarget,
+                ref positionVelocity,
+                followSmooth
+            );
+
+            directionToTarget = target.position - transform.position;
+            if (directionToTarget.magnitude < minDistanceToTarget)
+                transform.position = target.position - directionToTarget.normalized * minDistanceToTarget;
+
+            lookAhead = Vector3.SmoothDamp(
+                lookAhead,
+                target.forward * lookAheadDistance,
+                ref lookAheadVelocity,
+                lookAheadSmooth
+            );
+
+            transform.LookAt(target.position + lookAhead);
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        }
+
+        else
+        {
+            transform.position = Vector3.SmoothDamp(
+                transform.position,
+                target.position,
+                ref positionVelocity,
+                followSmooth
+            );
+
+            transform.LookAt(target.position);
+        }
     }
+
+
+
+
+
+
 }
